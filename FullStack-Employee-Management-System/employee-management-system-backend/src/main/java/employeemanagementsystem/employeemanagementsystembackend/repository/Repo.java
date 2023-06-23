543816b2b065f701c9a84735d65d2ea7509e1083
Omer <121943450+Mruruc@users.noreply.github.com>
2023-06-24 01:27:12 +0200

    Delete FullStack-Employee-Management-System directory

--- FullStack-Employee-Management-System/employee-management-system-backend/src/main/java/employeemanagementsystem/employeemanagementsystembackend/repository/Repo.java
+++
@@ -1,9 +0,0 @@
-package employeemanagementsystem.employeemanagementsystembackend.repository;
-
-import employeemanagementsystem.employeemanagementsystembackend.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Repo extends JpaRepository<Employee,Long> {
-}
